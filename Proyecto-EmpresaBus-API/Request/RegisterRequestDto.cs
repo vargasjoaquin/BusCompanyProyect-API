a145300d8c0f286ca2962e38972b98b2df1588af
Joaquin Vargas <vargasjoaquin9978@gmail.com>
2026-02-22 03:34:55 -0300

    validacion de longitud de telefono y dni

--- Proyecto-EmpresaBus-API/Request/RegisterRequestDto.cs
+++ Proyecto-EmpresaBus-API/Request/RegisterRequestDto.cs
@@ -8,12 +8,14 @@ namespace Proyecto_EmpresaBus_API.Request
         public string Email { get; set; }
         [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
         public string Password { get; set; }
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
         public string DNI { get; set; }
         public string? Rol { get; set; }
         public string? Ciudad { get; set; }
         public int? ProvinciaID { get; set; }
         public int? LocalidadID { get; set; }
         public string? Direccion { get; set; }
+        [StringLength(13, ErrorMessage = "El teléfono no puede superar los 13 caracteres.")]
         public string? Telefono { get; set; }
 
         public int? Edad { get; set; }
